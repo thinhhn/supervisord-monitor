387e252b33bdcd19dff99c6a7a49018ddcaf030b
Edgar Ho <thinhhn@boxme.asia>
2021-09-24 08:17:40 +0000

    Push

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -5,7 +5,6 @@
 	<title>Omisell Supervisord Monitoring</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url('/css/bootstrap.min.css');?>"/>
-	<link type="text/css" rel="stylesheet" href="<?php echo base_url('/css/bootstrap-responsive.min.css');?>"/>
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url('/css/custom.css');?>"/>
 <script type="text/javascript" src="<?php echo base_url('/js/jquery-1.10.1.min.js');?>"></script>
 <script type="text/javascript" src="<?php echo base_url('/js/bootstrap.min.js');?>"></script>
